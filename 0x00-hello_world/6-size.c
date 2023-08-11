157d4fe90485b75074c724ef47ffe526da8a281d
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-10 17:13:00 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-	printf("Size of a: %lu byte(s)\n",(unsigned long)sizeof(a));
-	printf("Size of b: %lu byte(s)\n",(unsigned long)sizeof(b));
-	printf("Size of c: %lu byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of d: %lu byte(s)\n",(unsigned long)sizeof(d));
-	printf("Size of e: %lu byte(s)\n",(unsigned long)sizeof(e));
-	return(0);
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of b: %lu byte(s)\n",(unsigned long)sizeof(b));
+printf("Size of c: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of d: %lu byte(s)\n",(unsigned long)sizeof(d));
+printf("Size of e: %lu byte(s)\n",(unsigned long)sizeof(e));
+return(0);
 }
+
