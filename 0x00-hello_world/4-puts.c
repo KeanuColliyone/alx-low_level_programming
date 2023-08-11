aff81aeefbf4c10efcde69e5e494f4d69e17e1ad
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-10 17:04:19 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char message[] = "\"Programming is like building a multilingual puzzle";
-	
-	puts(message);
-	
-	return (0);
+    char message[] = "\"Programming is like building a multilingual puzzle";
+
+    puts(message);
+
+    return (0);
 }
