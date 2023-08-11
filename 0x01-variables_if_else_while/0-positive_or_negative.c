a3ad428f4ca0cefbeec5548710dd8d05221570f9
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-11 11:49:51 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,11 @@ int min=-100;
 int max=100;
 int randNum = min + rand() % (max - min + 1);
 if(randNum > 0)
-{printf("%d is positive\n", randNum)
+{printf("%d is positive\n", randNum);
 }else if (randNum < 0)
-{printf("%d is negative\n", randNum)
+{printf("%d is negative\n", randNum);
 }else
-{printf("%d is zero\n", randNum)
+{printf("%d is zero\n", randNum);
 }
 return (0);
 }
