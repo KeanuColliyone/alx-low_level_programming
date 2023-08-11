52a45d117072e82dc415b8994a67bb14db0a8afe
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-10 17:52:02 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - standar error
+ *
+ * Retrun: 1 (Success)
+ */
 int main(void)
 {
-int message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-for (int i = 0; message[i] != '\0'; i++) {
-putchar(message[i]);
-}
-return (0);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
+return (1);
 }
 
