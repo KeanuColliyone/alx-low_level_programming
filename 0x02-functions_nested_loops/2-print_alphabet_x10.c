1a9732bb6ac9d861e1d9186c932b4f73a0d6db7f
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-15 09:04:55 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print all alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+        char i[] = "abcdefghijklmnopqrstuvwxyz";
+        int j = 0;
+        int k = 0;
+
+        while (i[j] != '\0')
+        {
+                _putchar(i[j]);
+                j++;
+                if (j == 26)
+                {
+                        _putchar('\n');
+                        if (k <= 10)
+                        {
+                            j = 0;
+                            k++;
+                        }
+                }
+        }
+}
+
