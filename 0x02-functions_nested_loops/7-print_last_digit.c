0332f68bb1a3c21d524c4263dcc214e82ed3b06f
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-15 11:15:45 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,24 +7,18 @@
  */
 int print_last_digit(int n)
 {
-int a;
+	int a;
 
-if (n < 0)
-{
-    n = -n;
-    a = n % 10;
-}
-else
-{
-    a = n % 10;
-}
+	if (n < 0)
+	n = -n;
 
-if (a < 0)
-{
-    a = -a;
-    _putchar(a + '0');
-}
+	a = n % 10;
 
-return (a);
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
 
