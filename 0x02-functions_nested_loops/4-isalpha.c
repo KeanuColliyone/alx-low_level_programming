86ce20a4f6ec6e95ea58616cb44f2e579cba97a2
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-15 10:21:22 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  * Return: 1 if char a letter, otherwise 0.
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z))
 	{
 		return (1);
 	}
