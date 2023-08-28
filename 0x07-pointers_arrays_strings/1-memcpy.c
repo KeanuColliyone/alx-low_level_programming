e42011a1f0966bcf420754b04b3fdb5a28aaf2fa
Kenan Kotzee <frostcolliyone@gmail.com>
2023-08-28 16:02:15 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-	int i = 0;
+	int i = n;
+	int j = 0;
 
-	for (; n > 0; i++)
+	for (; j < i; j++)
 	{
-		dest[i] = src[i];
+		dest[j] = src[j];
 		n--;
 	}
 
