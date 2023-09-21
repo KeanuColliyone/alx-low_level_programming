6e3560e59603283b22ad2edb29a46cf473a033fe
Kenan Kotzee <frostcolliyone@gmail.com>
2023-09-21 09:49:47 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,6 @@
  *
  * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
@@ -15,16 +14,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			s++;
-		}
+		h = h->next;
+		s++;
 	}
+
 	return (s);
 }
 
